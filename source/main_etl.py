1cbf415a7da65111b178ba527552e8ca47bb5471
dutaramadhan <duta2711@gmail.com>
2024-01-11 16:27:08 +0700

    feat: add automation for loading content from a folder

--- source/main_etl.py
+++ source/main_etl.py
@@ -2,12 +2,16 @@ import transform
 import load
 import os
 
-folder_path = r"D:\Kuliah\Kerja Praktik\Data Source\OneDrive_1_1-4-2024"
+folder_path = r"D:\Kuliah\Kerja Praktik\Data Source"
 for folder_name, sub_folders, files in os.walk(folder_path):
-    for file in files:
-        print(file)
-        path = folder_path + '/' + file
-        title, chunks = transform.transform(path)
-        source_id = load.insertSourceMetadata(file, file, title)
-        for index, chunk in enumerate(chunks):
-            load.insertChunkData(source_id, chunk)
+    for sub_folder in sub_folders:
+        for file in files:
+            print(file)
+            path = folder_path + '/' + sub_folder + '/' + file
+            if sub_folder == "Pasal":
+                title, chunks = transform.transform(path)
+            else:
+                title, chunks = transform.transformNonPasal(path)
+            source_id = load.insertSourceMetadata(file, file, title)
+            for index, chunk in enumerate(chunks):
+                load.insertChunkData(source_id, chunk)
