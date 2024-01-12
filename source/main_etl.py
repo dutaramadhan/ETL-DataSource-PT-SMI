59c4b8337ecd45b9da6224ece73b204467f0527e
dutaramadhan <duta2711@gmail.com>
2024-01-12 14:35:40 +0700

    benerin buat load data

--- source/main_etl.py
+++ source/main_etl.py
@@ -5,13 +5,15 @@ import os
 folder_path = r"D:\Kuliah\Kerja Praktik\Data Source"
 for folder_name, sub_folders, files in os.walk(folder_path):
     for sub_folder in sub_folders:
-        for file in files:
-            print(file)
-            path = folder_path + '/' + sub_folder + '/' + file
-            if sub_folder == "Pasal":
-                title, chunks = transform.transform(path)
-            else:
-                title, chunks = transform.transformNonPasal(path)
-            source_id = load.insertSourceMetadata(file, file, title)
-            for index, chunk in enumerate(chunks):
-                load.insertChunkData(source_id, chunk)
+        print(sub_folder)
+        for folder_name, sub_folders, files in os.walk(folder_path+'/'+sub_folder):
+            for file in files:
+                print(file)
+                path = folder_path + '/' + sub_folder + '/' + file
+                if sub_folder == "Pasal":
+                    title, chunks = transform.transform(path)
+                else:
+                    title, chunks = transform.transformNonPasal(path)
+                source_id = load.insertSourceMetadata(file, file, title)
+                for index, chunk in enumerate(chunks):
+                    load.insertChunkData(source_id, chunk)
